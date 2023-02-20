b3609d30838748360f2ba5c9518b0511e9cc0d64
changju <changju7857@gmail.com>
2023-02-20 16:20:37 +0900

    Java_work

--- Java_MCJ/src/basic/AssignmentOperator.java
+++ Java_MCJ/src/basic/AssignmentOperator.java
@@ -14,7 +14,7 @@ public class AssignmentOperator {
 		int a = 5;
 		int b = 5;
 		
-		a += 3; //a= a + 3
+		a += 3; //a = a + 3
 		System.out.println(a);
 		System.out.println(a);
 		System.out.println("-------------------------");
