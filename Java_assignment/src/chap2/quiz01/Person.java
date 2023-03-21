3f5acda8bfbadb12755e264e5c154819109a006c
Changju <changju7857@gmail.com>
2023-03-21 19:27:52 +0900

    2023.03.21

--- Java_assignment/src/chap2/quiz01/Person.java
+++ Java_assignment/src/chap2/quiz01/Person.java
@@ -8,30 +8,24 @@ public class Person {
 	public Person(String name, int age) {
 	}
 	
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public int getAge() {
 		return age;
 	}
-
 
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-
 	public void info() {
 		System.out.printf("이름: %s 나이: %d세 ", name, age);
 	}
-	
 
 }
